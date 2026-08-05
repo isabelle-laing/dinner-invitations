d4f3f8735ec6b3fdfacb12ef9d264c0ef3ec99e3
isabelle-laing <laingisabelle@gmail.com>
2026-08-05 10:29:26 +0800

    Add styles for menu image
    Add CSS styles for menu image in script.js

--- script.js
+++ script.js
@@ -157,4 +157,13 @@ form.addEventListener("submit", async (event) => {
     submitButton.disabled = false;
     submitButton.textContent = "Send my RSVP";
   }
+  
+  .menu-image {
+  display: block;
+  width: min(100%, 700px);
+  height: auto;
+  margin: 20px auto 10px;
+  }
+
+  
 });
